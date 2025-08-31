6cba8a845bcb89e1eb4c3e2198c905212232a6d0
samitkumarpatel <samitkumarpatel@gmail.com>
2025-08-31 22:51:42 +0200

    cleanup hook with partial python code

--- hook/remove-txt.sh
+++ hook/remove-txt.sh
@@ -3,3 +3,6 @@
 echo "Manual DNS Cleanup Hook"
 echo "Remove TXT record _acme-challenge.$CERTBOT_DOMAIN from your DNS provider."
 
+python3 -c "import requests; response = requests.delete('https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/personal/providers/Microsoft.Network/dnsZones/my-school.online/TXT/_acme-challenge?api-version=2018-05-01', headers={'Authorization': 'Bearer $accessToken'}); print(f'Status: {response.status_code}')"
+
+
